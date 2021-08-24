99461b63a71dfd359f7c8b34bd6b5813a5919a8c
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2021-08-25 06:10:01 +0700

    Add clear vgbs command

--- cogs/vgbs.py
+++ cogs/vgbs.py
@@ -50,16 +50,17 @@ class Vgbs(commands.Cog):
                         get_guild = self.bot.get_guild(id=config.gbs.guild_watch)
                         if get_guild:
                             gbs_role = discord.utils.get(ctx.guild.roles, name=config.gbs.role_name)
-                            if gbs_role and gbs_role not in ctx.author.roles:
-                                await ctx.author.add_roles(gbs_role)
-                                await ctx.send(f'{ctx.author.mention} You are verified now. Thank you!')
-                            elif gbs_role and gbs_role in ctx.author.roles:
-                                await ctx.send(f'{ctx.author.mention} You already verified.')
                             # Message should be delete after success
                             try:
                                 await ctx.message.delete()
                             except Exception as e:
                                 print(traceback.format_exc())
+
+                            if gbs_role and gbs_role not in ctx.author.roles:
+                                await ctx.author.add_roles(gbs_role)
+                                await ctx.send(f'{ctx.author.mention} You are verified now. Thank you!')
+                            elif gbs_role and gbs_role in ctx.author.roles:
+                                await ctx.send(f'{ctx.author.mention} You already verified.')
                         return
                 except Exception as e:
                     print(traceback.format_exc())
@@ -98,13 +99,51 @@ class Vgbs(commands.Cog):
                                 else:
                                     await ctx.send(f'{ctx.author.mention} You have been verified now but you have a role already. Thank you!')
                     else:
-                        await ctx.send(f'{ctx.author.mention} OK, address: `{address}` owns some doggo here but not yet verified ownership. Please sign and put your discord ID here: {config.gbs.verify_link}')
+                        await ctx.send(f'{ctx.author.mention} OK, address: `{address}` owns some doggo here but not yet verified ownership. Please sign and put your discord ID here: {config.gbs.verify_link}\nYour Discord ID: `{str(ctx.author.id)}`')
+                        # Message should be delete after success
+                        try:
+                            await ctx.message.delete()
+                        except Exception as e:
+                            print(traceback.format_exc())
                     return
                 except Exception as e:
                     print(traceback.format_exc())
         except Exception as e:
             print(traceback.format_exc())
 
+
+    @commands.has_permissions(manage_messages=True)
+    @commands.command(usage='clear', description="Clear all .vgbs command in verified channel")
+    async def clear(self, ctx):
+        delete_message_text = ".VGBS"
+        if isinstance(ctx.channel, discord.DMChannel):
+            await ctx.send(f'{EMOJI_ERROR} {ctx.author.mention} This command can not be in private.')
+            return
+        # If it is not in verified channel
+        if ctx.channel.id != config.gbs.verify_channel:
+            try:
+                await ctx.message.delete()
+                await ctx.send(f'{EMOJI_ERROR} {ctx.author.mention} Please use <#{str(config.gbs.verify_channel)}> only.')
+            except Exception as e:
+                print(traceback.format_exc())
+            return
+
+        count = 0
+        try:
+            messages = await ctx.channel.history(limit=1000).flatten()
+            if messages and len(messages) > 0:
+                for each in messages:
+                    if each.content.upper().startswith(delete_message_text):
+                        count += 1
+                        try:
+                            await each.delete()
+                        except Exception as e:
+                            print(traceback.format_exc())
+                            break
+                            return
+                await ctx.author.send(f'{ctx.author.mention} Found {str(count)} message(s) started with {delete_message_text} in {ctx.channel.mention} and deleted.')
+        except Exception as e:
+            print(traceback.format_exc())
 
 def setup(bot):
     bot.add_cog(Vgbs(bot))
